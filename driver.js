ee691d62313ca24f61c6a5ec3607a4d788a2acaf
Aman Prasad <amanprasad.9576@gmail.com>
2024-10-08 00:03:00 +0530

    add read,write,append method in file

--- driver.js
+++ driver.js
@@ -1,5 +1,21 @@
-const search_lib =require('./search.js');
+//const search_lib =require('./search.js');
 
-console.log(search_lib);
+//console.log(search_lib);
+const fs = require('fs/promises');
+
+async function readSampleFile(){
+    const data = await fs.readFile('./sample.txt','utf-8');
+    console.log(data);
+}
+readSampleFile();
+
+async function writeSampleFile(){
+await fs.writeFile('./sample.txt', 'test');
+}
 
 
+async function appendSampleFile(){
+    await fs.appendFile('./sample.txt','pass');
+}
+appendSampleFile();
+readSampleFile();
